5d84bcac89566befbb69a6a3fa979f773db59b21
Arushi <arushipati22@gmail.com>
2025-07-01 00:28:06 +0530

    made an api to see photo from url

--- src/main/java/com/cringebook/app/controllers/AddPhoto.java
+++ src/main/java/com/cringebook/app/controllers/AddPhoto.java
@@ -10,13 +10,17 @@ import com.cringebook.app.repository.PhotoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 @RestController
 public class AddPhoto {
@@ -45,38 +49,57 @@ public class AddPhoto {
         }return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
+    @GetMapping("/get_photo_from_url")
+    public ResponseEntity<byte[]> getPhotoFromUrl(@RequestHeader(HttpHeaders.AUTHORIZATION) String jwtToken, String url) throws IOException {
+
+        System.out.println("path =  " + "C:\\Users\\arushi\\Documents\\app\\app\\uploads\\"+ url);
+        File imgFile = new File("C:\\Users\\arushi\\Documents\\app\\app\\uploads\\" + url);
+
+        byte[]  imageByte = Files.readAllBytes(imgFile.toPath());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+
+        return new ResponseEntity<>(imageByte, headers, HttpStatus.OK);
+    }
+
     @PostMapping("/save_photos")
     public ResponseEntity<Integer> savePhoto(@RequestHeader(HttpHeaders.AUTHORIZATION) String jwtToken, String photo, Integer episodeId, MultipartFile image){
         if (image.isEmpty()){
             return new ResponseEntity<>(0, HttpStatus.NOT_FOUND);
         }
-        try{
-        System.out.println(image.getName() + "  " + image.getSize() + "  " + image.getOriginalFilename());
-        System.out.println("episodeId " + episodeId );
+        try {
+            String uuid = UUID.randomUUID().toString();
+//        System.out.println(image.getName() + "  " + image.getSize() + "  " + image.getOriginalFilename());
+            System.out.println(image.getName() + "  " + image.getSize() + "  " + uuid);
+            System.out.println("episodeId " + episodeId);
 
-        // saving uploaded file to directory
+            // saving uploaded file to directory
 
-        String uploadDir = "uploads/";
-        File directory = new File(uploadDir);
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-
-        String filepath = "C:\\Users\\arushi\\Documents\\app\\app\\uploads\\" + image.getOriginalFilename();
-        image.transferTo(new File(filepath));}
-        catch (Exception e){System.out.println(e.toString());}
-
-
-        Integer user_id = authentication.getIdFromToken(jwtToken);
-        Integer epiUserId = photoRepo.getUserIdForEpisodeId(episodeId);
-        if (user_id !=0  && Objects.equals(epiUserId, user_id)){
-            Photo photoObj = new Photo(episodeId, photo);
-            try{
-                Photo insertedPhoto = photoRepo.save(photoObj);
-                return new ResponseEntity<>(insertedPhoto.getPhoto_id(), HttpStatus.OK);
-            } catch (Exception e) {
-                return new ResponseEntity<>(0, HttpStatus.FORBIDDEN);
+            String uploadDir = "uploads/";
+            File directory = new File(uploadDir);
+            if (!directory.exists()) {
+                directory.mkdirs();
             }
-        }return new ResponseEntity<>(0, HttpStatus.OK);
+
+            String filepath = "C:\\Users\\arushi\\Documents\\app\\app\\uploads\\" + uuid + image.getOriginalFilename();
+            image.transferTo(new File(filepath));
+
+
+            Integer user_id = authentication.getIdFromToken(jwtToken);
+            Integer epiUserId = photoRepo.getUserIdForEpisodeId(episodeId);
+            if (user_id != 0 && Objects.equals(epiUserId, user_id)) {
+                Photo photoObj = new Photo(episodeId, uuid + image.getOriginalFilename());
+                try {
+                    Photo insertedPhoto = photoRepo.save(photoObj);
+                    return new ResponseEntity<>(insertedPhoto.getPhoto_id(), HttpStatus.OK);
+                } catch (Exception e) {
+                    return new ResponseEntity<>(0, HttpStatus.FORBIDDEN);
+                }
+            }
+            return new ResponseEntity<>(0, HttpStatus.OK);
+        }
+        catch (Exception e){System.out.println(e.toString());}
+        return new ResponseEntity<>(0, HttpStatus.OK);
     }
 }
